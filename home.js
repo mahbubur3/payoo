9c531d26796a34c42f001c4ae55efef7e1ed8d2d
mahbubur3 <mahbubur7716@gmail.com>
2025-10-24 17:09:33 +0600

    updated

--- home.js
+++ home.js
@@ -1,12 +1,26 @@
+const validPin = 1234;
+
+// Add money functionality
 document.getElementById('add-money-btn').addEventListener('click', function(event) {
     event.preventDefault();
     
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById("account-number").value;
     const amount = parseInt(document.getElementById("amount").value);
-    const pinNumber = document.getElementById("pin-number").value;
+    const pinNumber = parseInt(document.getElementById("pin-number").value);
 
     const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+
+    if (accountNumber.length < 11) {
+        alert("provide valid account number");
+        return;
+    }
+
+    if (pinNumber != validPin) {
+        alert("provide valid pin number");
+        return;
+    }
+
     const totalAvailableBalance = amount + availableBalance;
 
     // Set amount in deshboard
